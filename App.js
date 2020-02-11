a686e8f8d96045d19073e5f894accc1b2406fb8b
selvakumar-v <selva.va@yahoo.com>
2020-02-12 00:55:03 +0800

    Mount navigators in app

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
-import AppContainer from './src/navigation';
+//import AppContainer from './src/navigation';
+import AuthNavigator from './src/navigation/AuthNavigator';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AppContainer />
+      {true ? (
+        <>
+        <AuthNavigator />
+        </>
+      ) : (
+        <AppNavigator />
+      )}
     </NavigationContainer>
   );
 }
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import Dashboard from '../screens/Dashboard';
 import NewRequest from '../screens/NewRequest';
 import ViewRequest from '../screens/ViewRequest';
@@ -8,7 +9,28 @@ const Tab = createBottomTabNavigator();
 
 function AppNavigator() {
   return(
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if(route.name === 'Home') {
+            iconName = 'ios-home'
+          } else if(route.name === 'New Request') {
+            iconName = 'ios-add-circle'
+          } else if (route.name === 'View Request') {
+            iconName = 'ios-open'
+          } else if (route.name === 'Logout') {
+            iconName = 'ios-power'
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        }
+      })}
+      tabBarOptions={{
+        activeTintColor: '#d0021b',
+        inactiveTintColor: '#0967a1',
+      }}
+    >
       <Tab.Screen name='Home' component={Dashboard} />
       <Tab.Screen name="New Request" component={NewRequest} />
       <Tab.Screen name="View Request" component={ViewRequest} />
